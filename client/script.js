44ebf1f34ad78a0bbff9be2fe661450e7decf1cb
Sazidul0 <sazidulislam61@gmail.com>
2023-01-13 21:37:01 +0600

    backend deployed

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //Fetch data from server -> Bot's response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://friday-t15l.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
